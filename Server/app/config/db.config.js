a3df58c0920d3ef3724c48fd9425a2ba7765e0d0
jiebei <jiebeichai@gmail.com>
2021-09-30 11:53:09 +0800

    add db config

---
+++ Server/app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: "localhost",
+    USER: "postgres",
+    PASSWORD: "password",
+    DB: "DBSMarketplace", // this will be the database name
+    dialect: "postgres",
+    pool: {
+        max: 5, // mac number of connection in pool
+        min: 0, // min number of connection in pool
+        acquire: 30000, // max time, in ms that a connection can be idle before being released
+        idle: 10000 // max time, in ms that a pool will try to get connection before throwing error
+    }
+};
